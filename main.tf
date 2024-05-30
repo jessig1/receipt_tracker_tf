702d706f10f16f57ebd9ad679df431fe4962043b
Jakob Essig <essigj88@outlook.com>
2024-05-30 18:42:47 -0400

    Created resource group

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "example" {
-  name     = "example"
-  location = "West Europe"
+resource "azurerm_resource_group" "res-01" {
+  name     = "jwe-receipt-tracker"
+  location = "East US"
 }
