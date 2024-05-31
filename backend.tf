69f5eb627259f4280fc7b56c936504d1b4865fef
Jakob Essig <essigj88@outlook.com>
2024-05-30 22:35:13 -0400

    setup

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = azurerm_resource_group.res-01.name
-#     storage_account_name = azurerm_storage_account.res-01.name                  
-#     container_name       = azurerm_storage_container.res-01.name                      
-#     key                  = "initial.tfstate"        
-#   }
-# }
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "jwetfstatesa"                 
+    container_name       = "tfstate"                    
+    key                  = "initial.tfstate"        
+  }
+}
--- main.tf
+++ main.tf
@@ -4,25 +4,15 @@ resource "azurerm_resource_group" "res-01" {
 }
 
 resource "azurerm_container_registry" "acr" {
+  depends_on = [
+    azurerm_resource_group.res-01
+  ]
+
   name                = "receipttrackerregister"
   resource_group_name = azurerm_resource_group.res-01.name
   location            = var.location
   sku                 = "Basic"
   admin_enabled       = false
-}
-
-resource "azurerm_storage_account" "res-01" {
-  name                     = "receipttrackersa"
-  resource_group_name      = azurerm_resource_group.res-01.name
-  location                 = azurerm_resource_group.res-01.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
-}
-
-resource "azurerm_storage_container" "res-01" {
-  name                  = "receipttrackertfstate"
-  storage_account_name  = azurerm_storage_account.res-01.name
-  container_access_type = "private"
 }
 
 # resource "azurerm_service_plan" "res-01" {
