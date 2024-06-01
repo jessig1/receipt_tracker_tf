0ff814733eab5ff3985c1ca57a232028271ab4eb
Jake <essigj88@outlook.com>
2024-06-01 13:07:16 -0400

    app service

--- main.tf
+++ main.tf
@@ -15,19 +15,19 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = false
 }
 
-# resource "azurerm_service_plan" "res-01" {
-#   name                = "example"
-#   resource_group_name = azurerm_resource_group.res-01.name
-#   location            = var.location
-#   os_type             = "Linux"
-#   sku_name            = "P1v2"
-# }
+resource "azurerm_service_plan" "res-01" {
+  name                = "receipttrackerasp"
+  resource_group_name = azurerm_resource_group.res-01.name
+  location            = var.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
 
-# resource "azurerm_linux_web_app" "res-01" {
-#   name                = "example"
-#   resource_group_name = azurerm_resource_group.res-01.name
-#   location            = var.location
-#   service_plan_id     = azurerm_service_plan.res-01.id
+resource "azurerm_linux_web_app" "res-01" {
+  name                = "receipttrackerapp"
+  resource_group_name = azurerm_resource_group.res-01.name
+  location            = var.location
+  service_plan_id     = azurerm_service_plan.res-01.id
 
-#   site_config {}
-# }
+  site_config {}
+}
