f6c9c774b017db870bc64b97fa175d2bb02e0ee8
Jakob Essig <essigj88@outlook.com>
2024-06-05 21:52:05 -0400

    fix

--- main.tf
+++ main.tf
@@ -39,6 +39,10 @@ resource "azurerm_service_plan" "res-01" {
 
 resource "azurerm_linux_web_app" "res-01" {
   name                = "receipttrackerapp"
+  depends_on = [
+    azurerm_service_plan.res-01
+  ]
+
   resource_group_name = azurerm_resource_group.res-01.name
   location            = var.location
   service_plan_id     = azurerm_service_plan.res-01.id
